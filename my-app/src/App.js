b9fc7be98dec7c4cb9d92d2cb151bc3f60b59202
Patt Höge <paty.hoge98@gmail.com>
2024-01-07 11:49:32 +0000

    add class components style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+
+// import Sidebar from './components/Sidebar';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
     {/* Add your components here */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      {/* <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/> */}
+      {/* <StatefulGreetingWithCallback /> */}
+      {/* <StatefulGreetingWithPrevState /> */}
+      <NavBarSimple />
     </div>
   );
 }
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,41 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css";
+
+
+class NavBarSimple extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello, guest",
+            buttonText: "LogIn",
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState);
+            console.log("Previous Props:", prevProps);
+            return {
+              message: prevState.message === "Hello, guest" ? "Welcome Back" : "Hello, guest",
+              buttonText:  prevState.buttonText === "LogIn" ? "LogOut" : "LogIn",
+            };
+          });
+    }
+
+    render() {
+        return (
+            <div className="{css.NavBar}">
+                <h1>My Gallery </h1>
+                <span>
+                {this.state.message}
+                </span>
+                <button onClick={() => this.handleClick()}>
+                    {this.state.buttonText}
+                </button>
+            </div>
+        ) 
+    }
+}
+
+export default NavBarSimple;
